0802dfe2e4bddba0f9d6c9d6d389da09b79b11cf
wemb446 <piyopiyosh1101@gmail.com>
2020-09-06 00:35:59 +0900

    hello ts-react

---
+++ frontend/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ 
+// コンポーネント読み込み
+import App from "./components/App";
+ 
+ 
+ReactDOM.render(<App />, document.getElementById("root"));
+
